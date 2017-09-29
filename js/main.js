d3d150b6a3fdc648dd80cf508f6ec6a2a3ba39da
Evgenii Levashov <evgenii.levashov@gmail.com>
2017-09-29 19:22:14 +0300

    Начинаем программировать

--- js/main.js
+++ js/main.js
@@ -1,33 +1,28 @@
-let mainBox = document.querySelector('main.central');
-let screensArray = Array.from(document.querySelectorAll('template'));
+const mainBox = document.querySelector('main.central');
+const screensArray = Array.from(document.querySelectorAll('template'));
 let screens = [];
 
-screensArray.forEach(screenTemplate => {
-  screens.push(screenTemplate.innerHTML);
+const ARROW_LEFT_KEY = 37;
+const ARROW_RIGHT_KEY = 39;
+
+screensArray.map(screenTemplate => {
+  return screens.push(screenTemplate.innerHTML);
 });
 
-let showScreen = screenNumber => {
-  mainBox.innerHTML = screens[screenNumber];
-};
+let lastScreenIndex = screens.length - 1;
+
+const showScreen = screenNumber => mainBox.innerHTML = screens[screenNumber];
 
 let index = 0;
 showScreen(index);
 
 document.addEventListener(`keydown`, (e) => {
-  if (e.altKey && e.keyCode === 37) {
-    if (index > 0) {
-      index--;
-    } else {
-      index = screens.length - 1;
-    }
+  if (e.altKey && e.keyCode === ARROW_LEFT_KEY) {
+    index = (index > 0) ? index - 1 : lastScreenIndex;
     showScreen(index);
   }
-  if (e.altKey && e.keyCode === 39) {
-    if (index < screens.length - 1) {
-      index++;
-    } else {
-      index = 0;
-    }
+  if (e.altKey && e.keyCode === ARROW_RIGHT_KEY) {
+    index = (index < lastScreenIndex) ? index + 1 : 0;
     showScreen(index);
   }
 });
