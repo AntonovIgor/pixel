7ffdce80ba3d80d8ac5ce4ea0c685d6cb6e51d1b
Evgenii Levashov <evgenii.levashov@gmail.com>
2017-10-25 01:15:32 +0300

    поменял формат вопросов

--- js/data/fakeQuestions.js
+++ js/data/fakeQuestions.js
@@ -1,370 +1,217 @@
-// export default [
-//   {
-//     type: `guessEach`,
-//     width: 468,
-//     height: 458,
-//     options: [
-//       {
-//         source: `http://placehold.it/468x458`,
-//         isPhoto: false
-//       },
-//       {
-//         source: `http://placehold.it/468x458`,
-//         isPhoto: true
-//       }
-//     ]
-//   },
-//   {
-//     type: `guessWide`,
-//     width: 705,
-//     height: 455,
-//     options: [
-//       {
-//         source: `http://placehold.it/705x455`,
-//         isPhoto: false
-//       }
-//     ]
-//   },
-//   {
-//     type: `guessTriple`,
-//     width: 304,
-//     height: 455,
-//     options: [
-//       {
-//         source: `http://placehold.it/304x455`,
-//         isPhoto: true
-//       },
-//       {
-//         source: `http://placehold.it/304x455`,
-//         isPhoto: false
-//       },
-//       {
-//         source: `http://placehold.it/304x455`,
-//         isPhoto: true
-//       }
-//     ]
-//   },
-//   {
-//     type: `guessEach`,
-//     width: 468,
-//     height: 458,
-//     options: [
-//       {
-//         source: `http://placehold.it/468x458`,
-//         isPhoto: false
-//       },
-//       {
-//         source: `http://placehold.it/468x458`,
-//         isPhoto: true
-//       }
-//     ]
-//   },
-//   {
-//     type: `guessWide`,
-//     width: 705,
-//     height: 455,
-//     options: [
-//       {
-//         source: `http://placehold.it/705x455`,
-//         isPhoto: false
-//       }
-//     ]
-//   },
-//   {
-//     type: `guessTriple`,
-//     width: 304,
-//     height: 455,
-//     options: [
-//       {
-//         source: `http://placehold.it/304x455`,
-//         isPhoto: true
-//       },
-//       {
-//         source: `http://placehold.it/304x455`,
-//         isPhoto: false
-//       },
-//       {
-//         source: `http://placehold.it/304x455`,
-//         isPhoto: true
-//       }
-//     ]
-//   },
-//   {
-//     type: `guessEach`,
-//     width: 468,
-//     height: 458,
-//     options: [
-//       {
-//         source: `http://placehold.it/468x458`,
-//         isPhoto: false
-//       },
-//       {
-//         source: `http://placehold.it/468x458`,
-//         isPhoto: true
-//       }
-//     ]
-//   },
-//   {
-//     type: `guessWide`,
-//     width: 705,
-//     height: 455,
-//     options: [
-//       {
-//         source: `http://placehold.it/705x455`,
-//         isPhoto: false
-//       }
-//     ]
-//   },
-//   {
-//     type: `guessTriple`,
-//     width: 304,
-//     height: 455,
-//     options: [
-//       {
-//         source: `http://placehold.it/304x455`,
-//         isPhoto: true
-//       },
-//       {
-//         source: `http://placehold.it/304x455`,
-//         isPhoto: false
-//       },
-//       {
-//         source: `http://placehold.it/304x455`,
-//         isPhoto: true
-//       }
-//     ]
-//   },
-//   {
-//     type: `guessEach`,
-//     width: 468,
-//     height: 458,
-//     options: [
-//       {
-//         source: `http://placehold.it/468x458`,
-//         isPhoto: false
-//       },
-//       {
-//         source: `http://placehold.it/468x458`,
-//         isPhoto: true
-//       }
-//     ]
-//   }
-// ];
-
 export default [
   {
-    "type": "one-of-three",
-    "question": "Найдите фото среди изображений",
-    "answers": [
+    type: `one-of-three`,
+    question: `Найдите фото среди изображений`,
+    answers: [
       {
-        "image": {
-          "url": "https://k31.kn3.net/4BF6BBF0E.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://k31.kn3.net/4BF6BBF0E.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "painting"
+        type: `painting`
       },
       {
-        "image": {
-          "url": "https://k38.kn3.net/AD92BA712.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://k38.kn3.net/AD92BA712.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "painting"
+        type: `painting`
       },
       {
-        "image": {
-          "url": "https://i.redd.it/apoalsgb702y.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://i.redd.it/apoalsgb702y.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "photo"
+        type: `photo`
       }]
   },
   {
-    "type": "tinder-like",
-    "question": "Угадай, фото или рисунок?",
-    "answers": [{
-      "image": {
-        "url": "http://i.imgur.com/DKR1HtB.jpg",
-        "width": 705,
-        "height": 455
+    type: `tinder-like`,
+    question: `Угадай, фото или рисунок?`,
+    answers: [{
+      image: {
+        url: `http://i.imgur.com/DKR1HtB.jpg`,
+        width: 705,
+        height: 455
       },
-      "type": "photo"
+      type: `photo`
     }]
   },
   {
-    "type": "two-of-two",
-    "question": "Угадайте для каждого изображения фото или рисунок?",
-    "answers": [
+    type: `two-of-two`,
+    question: `Угадайте для каждого изображения фото или рисунок?`,
+    answers: [
       {
-        "image": {
-          "url": "https://k40.kn3.net/6A7A24F7C.jpg"
-          , "width": 468
-          , "height": 458
+        image: {
+          url: `https://k40.kn3.net/6A7A24F7C.jpg`
+          , width: 468
+          , height: 458
         }
-        , "type": "painting"
+        , type: `painting`
       },
       {
-        "image": {
-          "url": "http://i.imgur.com/gUeK0qE.jpg",
-          "width": 468,
-          "height": 458
+        image: {
+          url: `http://i.imgur.com/gUeK0qE.jpg`,
+          width: 468,
+          height: 458
         },
-        "type": "photo"
+        type: `photo`
       }]
   },
   {
-    "type": "one-of-three",
-    "question": "Найдите фото среди изображений",
-    "answers": [
+    type: `one-of-three`,
+    question: `Найдите фото среди изображений`,
+    answers: [
       {
-        "image": {
-          "url": "https://k31.kn3.net/4BF6BBF0E.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://k31.kn3.net/4BF6BBF0E.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "painting"
+        type: `painting`
       },
       {
-        "image": {
-          "url": "https://k38.kn3.net/AD92BA712.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://k38.kn3.net/AD92BA712.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "painting"
+        type: `painting`
       },
       {
-        "image": {
-          "url": "https://i.redd.it/apoalsgb702y.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://i.redd.it/apoalsgb702y.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "photo"
+        type: `photo`
       }]
   },
   {
-    "type": "tinder-like",
-    "question": "Угадай, фото или рисунок?",
-    "answers": [{
-      "image": {
-        "url": "http://i.imgur.com/DKR1HtB.jpg",
-        "width": 705,
-        "height": 455
+    type: `tinder-like`,
+    question: `Угадай, фото или рисунок?`,
+    answers: [{
+      image: {
+        url: `http://i.imgur.com/DKR1HtB.jpg`,
+        width: 705,
+        height: 455
       },
-      "type": "photo"
+      type: `photo`
     }]
   },
   {
-    "type": "two-of-two",
-    "question": "Угадайте для каждого изображения фото или рисунок?",
-    "answers": [
+    type: `two-of-two`,
+    question: `Угадайте для каждого изображения фото или рисунок?`,
+    answers: [
       {
-        "image": {
-          "url": "https://k40.kn3.net/6A7A24F7C.jpg"
-          , "width": 468
-          , "height": 458
+        image: {
+          url: `https://k40.kn3.net/6A7A24F7C.jpg`
+          , width: 468
+          , height: 458
         }
-        , "type": "painting"
+        , type: `painting`
       },
       {
-        "image": {
-          "url": "http://i.imgur.com/gUeK0qE.jpg",
-          "width": 468,
-          "height": 458
+        image: {
+          url: `http://i.imgur.com/gUeK0qE.jpg`,
+          width: 468,
+          height: 458
         },
-        "type": "photo"
+        type: `photo`
       }]
   },
   {
-    "type": "one-of-three",
-    "question": "Найдите фото среди изображений",
-    "answers": [
+    type: `one-of-three`,
+    question: `Найдите фото среди изображений`,
+    answers: [
       {
-        "image": {
-          "url": "https://k31.kn3.net/4BF6BBF0E.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://k31.kn3.net/4BF6BBF0E.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "painting"
+        type: `painting`
       },
       {
-        "image": {
-          "url": "https://k38.kn3.net/AD92BA712.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://k38.kn3.net/AD92BA712.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "painting"
+        type: `painting`
       },
       {
-        "image": {
-          "url": "https://i.redd.it/apoalsgb702y.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://i.redd.it/apoalsgb702y.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "photo"
+        type: `photo`
       }]
   },
   {
-    "type": "tinder-like",
-    "question": "Угадай, фото или рисунок?",
-    "answers": [{
-      "image": {
-        "url": "http://i.imgur.com/DKR1HtB.jpg",
-        "width": 705,
-        "height": 455
+    type: `tinder-like`,
+    question: `Угадай, фото или рисунок?`,
+    answers: [{
+      image: {
+        url: `http://i.imgur.com/DKR1HtB.jpg`,
+        width: 705,
+        height: 455
       },
-      "type": "photo"
+      type: `photo`
     }]
   },
   {
-    "type": "two-of-two",
-    "question": "Угадайте для каждого изображения фото или рисунок?",
-    "answers": [
+    type: `two-of-two`,
+    question: `Угадайте для каждого изображения фото или рисунок?`,
+    answers: [
       {
-        "image": {
-          "url": "https://k40.kn3.net/6A7A24F7C.jpg"
-          , "width": 468
-          , "height": 458
+        image: {
+          url: `https://k40.kn3.net/6A7A24F7C.jpg`
+          , width: 468
+          , height: 458
         }
-        , "type": "painting"
+        , type: `painting`
       },
       {
-        "image": {
-          "url": "http://i.imgur.com/gUeK0qE.jpg",
-          "width": 468,
-          "height": 458
+        image: {
+          url: `http://i.imgur.com/gUeK0qE.jpg`,
+          width: 468,
+          height: 458
         },
-        "type": "photo"
+        type: `photo`
       }]
   },
   {
-    "type": "one-of-three",
-    "question": "Найдите фото среди изображений",
-    "answers": [
+    type: `one-of-three`,
+    question: `Найдите фото среди изображений`,
+    answers: [
       {
-        "image": {
-          "url": "https://k31.kn3.net/4BF6BBF0E.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://k31.kn3.net/4BF6BBF0E.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "painting"
+        type: `painting`
       },
       {
-        "image": {
-          "url": "https://k38.kn3.net/AD92BA712.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://k38.kn3.net/AD92BA712.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "painting"
+        type: `painting`
       },
       {
-        "image": {
-          "url": "https://i.redd.it/apoalsgb702y.jpg",
-          "width": 304,
-          "height": 455
+        image: {
+          url: `https://i.redd.it/apoalsgb702y.jpg`,
+          width: 304,
+          height: 455
         },
-        "type": "photo"
+        type: `photo`
       }]
   }
 ];
