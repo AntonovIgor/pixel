cff3c49cf99aeb4ba2ffae133501047d1362eb75
Evgenii Levashov <evgenii.levashov@gmail.com>
2017-11-02 23:29:26 +0300

    hash

--- js/application.js
+++ js/application.js
@@ -4,29 +4,69 @@ import RulesScreen from './screens/screen-rules/screen-rules';
 import GameScreen from './screens/game/game';
 import StatsScreen from './screens/screen-stats/screen-stats';
 
+const ControllerId = {
+  INTRO: ``,
+  GREETING: `greeting`,
+  RULES: `rules`,
+  GAME: `game`,
+  STATS: `stats`
+};
+
+const routes = {
+  [ControllerId.INTRO]: new IntroScreen(),
+  [ControllerId.GREETING]: new GreetingScreen(),
+  [ControllerId.RULES]: new RulesScreen(),
+  [ControllerId.GAME]: new GameScreen(),
+  [ControllerId.STATS]: new StatsScreen()
+};
+
+const saveState = (state) => {
+  return JSON.stringify(state);
+};
+
+const loadState = (dataString) => {
+  try {
+    return JSON.parse(dataString);
+  } catch (e) {
+    return ``;
+  }
+};
+
 export default class Application {
+  static init() {
+    const hashChangeHandler = () => {
+      const hashValue = location.hash.replace(`#`, ``);
+      const [id, data] = hashValue.split(`?`);
+      this.changeHash(id, data);
+    };
+    window.onhashchange = hashChangeHandler;
+    hashChangeHandler();
+  }
+
+  static changeHash(id, data) {
+    const controller = routes[id];
+    if (controller) {
+      controller.init(loadState(data));
+    }
+  }
+
   static showIntro() {
-    const screen = new IntroScreen();
-    screen.init();
+    location.hash = ControllerId.INTRO;
   }
 
   static showGreeting() {
-    const screen = new GreetingScreen();
-    screen.init();
+    location.hash = ControllerId.GREETING;
   }
 
   static showRules() {
-    const screen = new RulesScreen();
-    screen.init();
+    location.hash = ControllerId.RULES;
   }
 
   static showGame() {
-    const screen = new GameScreen();
-    screen.init();
+    location.hash = ControllerId.GAME;
   }
 
   static showStats(state) {
-    const screen = new StatsScreen(state);
-    screen.init();
+    location.hash = `${ControllerId.STATS}?${saveState(state)}`;
   }
 }
